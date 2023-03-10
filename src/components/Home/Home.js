d1e086d5856f35187a305e8797e35ce6473675f7
GolamMuid <golammuid@gmail.com>
2023-03-10 19:45:26 +0600

    added countdown

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,9 +8,13 @@ function Home({ regAim, preAim }) {
         <img src={logo} alt="Logo" />
         <button className="start-button" onClick={regAim}>
           Regular Aim
+          <div className="start-button-helper-text">(Click LMB to shoot)</div>
         </button>
         <button className="start-button" onClick={preAim}>
           Pre Aim
+          <div className="start-button-helper-text">
+            (Hold RMB to aim and LMB to shoot)
+          </div>
         </button>
       </div>
     </div>
--- src/components/Target/TargetPreAim.js
+++ src/components/Target/TargetPreAim.js
@@ -10,25 +10,53 @@ function TargetPreAim({ score, setScore }) {
   const [xCoord, setxCoord] = useState(1);
   const [yCoord, setyCoord] = useState(1);
   const [zombieFace, setZombieFace] = useState(zombieArray[zombie]);
-
-  const handleScore = (e) => {
-    if (e.buttons === 3) {
-      setScore((prev) => prev + 1);
-    }
-  };
+  const [timerId, setTimerId] = useState(null);
 
   let x = window.innerWidth - 120;
   let y = window.innerHeight - 150;
 
-  console.log(xCoord, yCoord);
-
-  useEffect(() => {
+  const zombieSwitch = () => {
     setxCoord(Math.floor(Math.random() * (x - 20) + 20));
     setyCoord(Math.floor(Math.random() * (y - 50) + 50));
 
     zombie = Math.floor(Math.random() * 4);
     setZombieFace(zombieArray[zombie]);
+  };
+
+  useEffect(() => {
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+
+    const newTimerId = setInterval(() => {
+      // Fire your function here
+      zombieSwitch();
+    }, 2000);
+
+    setTimerId(newTimerId);
+
+    return () => {
+      clearInterval(newTimerId);
+    };
   }, [score]);
+
+  const resetTimer = () => {
+    if (timerId) {
+      clearInterval(timerId);
+    }
+
+    const newTimerId = setInterval(() => {}, 2000);
+
+    setTimerId(newTimerId);
+    zombieSwitch();
+  };
+
+  const handleScore = (e) => {
+    if (e.buttons === 3) {
+      setScore((prev) => prev + 1);
+      resetTimer();
+    }
+  };
 
   const buttonStyle = {
     position: "absolute",
